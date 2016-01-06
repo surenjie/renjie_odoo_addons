c43fcbdc738789b6d7b0d80d99ee71706f233272
Renjie <i@renjie.me>
2016-01-06 12:05:01 +0800

    add china icp beian info

--- website_china_features/models/res_config.py
+++ website_china_features/models/res_config.py
@@ -5,6 +5,8 @@ from openerp.osv import fields, osv
 class website_config_settings(osv.osv_memory):
     _inherit = 'website.config.settings'
     _columns = {
+        'china_icp_beian_info': fields.related('website_id', 'china_icp_beian_info', type="char", string='China ICP BeiAn Info'),
+
         'baidu_analytics_key': fields.related('website_id', 'baidu_analytics_key', type="char", string='Baidu Analytics Key'),
         'baidu_webmaster_push': fields.related('website_id', 'baidu_webmaster_push', type="boolean", string='Push web link to Baidu webmaster platform'),
         
--- website_china_features/models/website.py
+++ website_china_features/models/website.py
@@ -5,6 +5,8 @@ from openerp.osv import fields, osv
 class website(osv.osv):
     _inherit = "website"
     _columns = {
+        'china_icp_beian_info': fields.char('China ICP BeiAn Info'),
+
         'baidu_analytics_key': fields.char('Baidu Analytics Key'),
         'baidu_webmaster_push': fields.boolean('Baidu Webmaster Push'),
         
