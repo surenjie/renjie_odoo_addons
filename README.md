5e28bf7a1ab42a07036fdf79831842fce889d7be
srj <i@renjie.me>
2018-10-19 21:06:08 +0800

    Upgrade odoo12 available modules

--- README.md
+++ README.md
@@ -1,21 +1,13 @@
 # Renjie Odoo Addons:
 
-* [website_china_features](https://www.odoo.com/apps/modules/11.0/website_china_features/)(Build Your China Features Website)
+* [website_china_features](https://www.odoo.com/apps/modules/12.0/website_china_features/)(Build Your China Features Website)
 
     这是个长期维护的应用，因为中国不断的高速变化发展，未来特色完全不可预测，欢迎各种建议交流
 
-* [website_disabled](https://www.odoo.com/apps/modules/11.0/website_disabled/)(Disabled your website home '/' auto redirect to '/web')
+* [website_disabled](https://www.odoo.com/apps/modules/12.0/website_disabled/)(Disabled your website home '/' auto redirect to '/web')
 
     不想使用被动依赖安装的网站功能
 
-* [bank_china_features](https://www.odoo.com/apps/modules/11.0/bank_china_features/)(Bank account information with China features)
-
-    根据实际需要为您的银行账户添加有别于持有者的账户名称、分支行相关信息、是否对公账户属性
-
-* [product_multi_alias](https://www.odoo.com/apps/modules/11.0/product_multi_alias/)(Add more than one alias for your product)
+* [product_multi_alias](https://www.odoo.com/apps/modules/12.0/product_multi_alias/)(Add more than one alias for your product)
 
     根据实际情况为您的产品添加多个别名，详情请参照截图
-
-* [visible_search_menu](https://www.odoo.com/apps/modules/11.0/visible_search_menu/)(Default visible advanced search menu)
-
-    默认显示高级搜索菜单（筛选、分组、收藏）
--- website_china_features/models/res_config.py
+++ website_china_features/models/res_config.py
@@ -9,12 +9,12 @@ _logger = logging.getLogger(__name__)
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    china_icp_beian_info = fields.Char('China ICP BeiAn Info', related='website_id.china_icp_beian_info')
+    china_icp_beian_info = fields.Char('China ICP BeiAn Info', related='website_id.china_icp_beian_info', readonly=False)
 
-    baidu_analytics_key = fields.Char('Baidu Analytics Key', related='website_id.baidu_analytics_key')
-    baidu_webmaster_push = fields.Boolean('Push web link to Baidu webmaster platform', related='website_id.baidu_webmaster_push')
+    baidu_analytics_key = fields.Char('Baidu Analytics Key', related='website_id.baidu_analytics_key', readonly=False)
+    baidu_webmaster_push = fields.Boolean('Push web link to Baidu webmaster platform', related='website_id.baidu_webmaster_push', readonly=False)
     
-    social_wechat = fields.Char('WeChat Account', related='website_id.social_wechat')
-    social_qq = fields.Char('QQ Account', related='website_id.social_qq')
-    social_weibo = fields.Char('Weibo Account', related='website_id.social_weibo')
-    social_renren = fields.Char('Renren Account', related='website_id.social_renren')
+    social_wechat = fields.Char('WeChat Account', related='website_id.social_wechat', readonly=False)
+    social_qq = fields.Char('QQ Account', related='website_id.social_qq', readonly=False)
+    social_weibo = fields.Char('Weibo Account', related='website_id.social_weibo', readonly=False)
+    social_renren = fields.Char('Renren Account', related='website_id.social_renren', readonly=False)
