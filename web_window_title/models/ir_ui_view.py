377c2aba5c6e1c51398609f6acf253f4af3249de
srj <i@renjie.me>
2023-01-20 20:31:15 +0800

    Upgrade odoo16 web_window_title Mod

--- web_window_title/models/ir_ui_view.py
+++ web_window_title/models/ir_ui_view.py
@@ -10,9 +10,9 @@ class View(models.Model):
     _inherit = 'ir.ui.view'
 
     @api.model
-    def _render_template(self, template, values=None, engine='ir.qweb'):
+    def _render_template(self, template, values=None):
         if template in ['web.login', 'web.webclient_bootstrap']:
             if not values:
                 values = {}
             values["title"] = self.env['ir.config_parameter'].sudo().get_param("web.base.title", "")
-        return super(View, self)._render_template(template, values, engine)
+        return super(View, self)._render_template(template, values)
